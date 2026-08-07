c500c6e80fa40806fc05a4bfcf697c811254bce7
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-08 00:47:15 +0300

    feat: add SVG icons for social media and UI components
    - Added CloseIcon, FacebookIcon, InstagramIcon, LogoIcon, MenuIcon, PinterestIcon, TwitterIcon, and YoutubeIcon components.
    - Created an index file to export all icons for easier imports.
    - Implemented utility function `cn` for class name merging using clsx and tailwind-merge.

---
+++ src/components/icons/MenuIcon.tsx
@@ -0,0 +1,13 @@
+export default function MenuIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 25 18"
+      fill="currentColor"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M0 0h25v4H0zM0 7h25v4H0zM0 14h25v4H0z" />
+    </svg>
+  );
+}
---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as CloseIcon } from "./CloseIcon";
+export { default as FacebookIcon } from "./FacebookIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as LogoIcon } from "./LogoIcon";
+export { default as MenuIcon } from "./MenuIcon";
+export { default as PinterestIcon } from "./PinterestIcon";
+export { default as TwitterIcon } from "./TwitterIcon";
+export { default as YoutubeIcon } from "./YoutubeIcon";
